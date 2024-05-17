c27cd170be32b0aaa2a87032a4ee0bb1c1ececff
CesarOli <cesaraleoliveira@gmail.com>
2024-05-17 14:42:52 -0300

    Exemplo de documentação bem elaborada.

---
+++ documentacao/src/Comentarios.java
@@ -0,0 +1,11 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Ola eu sou um comentário em uma única linha
+
+
+        /*Olá
+         * Eu sou um comentário mais detalhado 
+         * quando necessário.
+         */
+    }
+}
