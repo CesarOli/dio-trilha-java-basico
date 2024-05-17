19f409536753605395d6a889fbc85beadee4f19f
CesarOli <cesaraleoliveira@gmail.com>
2024-05-17 16:20:03 -0300

    Exemplo de como não declarar variaveis e usar o espaço dos comentários para complementar um código mal implementado, com nomes de variaveis incompletas.

--- documentacao/src/Comentarios.java
+++ documentacao/src/Comentarios.java
@@ -5,7 +5,27 @@ public class Comentarios {
 
         /*Olá
          * Eu sou um comentário mais detalhado 
-         * quando necessário.
+         * quando necessário, e/ ou mais robusto.
          */
     }
-}
+    public void metodo(){
+        /*Este método foi elaborado as pressas
+         * por isso eu abrevei o nome das variáveis 
+         * mas olha, ele tem a finalidade de somar ou multiplicar 
+         * dois números
+         */
+        }
+        
+        public int somaMultiplica(int n, int x, String m){ 
+            int r = 0; // r é igual ao resultado
+            if(m == "M"){ // M= multiplicação
+                r = n * x;
+            }else{
+                // se não soma mesmo
+                r = n + x;
+            }
+            return r;
+            
+        }
+    }
+
