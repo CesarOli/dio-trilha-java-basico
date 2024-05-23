c0cf9d6e2fd333ea159c6a73175fd1faea1c0090
CesarOli <cesaraleoliveira@gmail.com>
2024-05-22 23:37:43 -0300

    Aplicação do conceito laço de repetição 'for'.

---
+++ controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for(int pulo = 1; pulo <= 20; pulo++) {
+            System.out.println("Contando pulos = " + pulo);
+        }
+
+        System.out.println("Cheguei ao meu objetivo!");
+    }
+}
