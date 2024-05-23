790752e45c7965b9a300adcdf0ec23857b0098a1
CesarOli <cesaraleoliveira@gmail.com>
2024-05-22 23:40:02 -0300

    Aplicação do conceito 'for' utilizando o conceito de 'array'.

---
+++ controle-de-fluxo/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"LARISSA", "GABRIELA", "JENNIFER", "GISLÂNIA"};
+
+        for (int x=0; x < alunos.length;x++) {
+            System.out.println("A aluna no indice x=" + x + " é " + alunos[x]);
+        }
+    }
+}
