38d2345181ac3397ba54e1a4318e59c02c7e3b92
CesarOli <cesaraleoliveira@gmail.com>
2024-05-26 23:58:33 -0300

    Aplicação de um segundo exemplo utilizandom conceito 'for' em arrays.

---
+++ controle-de-fluxo/src/SegundoExemploForArray.java
@@ -0,0 +1,9 @@
+public class SegundoExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"JENNIFER", "GISLÂNIA","LARISSA", "GABRIELA"};
+
+        for(String aluna : alunos){
+            System.out.println("Nome da aluna é: " + aluna);
+        }
+    }
+}
