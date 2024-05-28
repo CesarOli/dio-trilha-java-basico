c67032233fc22d182663b236c893eaf55b65bbdc
CesarOli <cesaraleoliveira@gmail.com>
2024-05-27 23:45:38 -0300

    Aplicação do segundo cenário do conceito 'if/else' em uma linha de código.

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,6 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        // Cenário 2
+        // Cenário 2 if / else
 
         int nota = 3;
         String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
